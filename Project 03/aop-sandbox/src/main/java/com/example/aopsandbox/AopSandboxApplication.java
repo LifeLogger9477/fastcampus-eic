d4f585e82aade5219827a6cb62e303e7e88f7d78
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-03-02 20:43:06 +0900

    Project 03. Ch 01. 06. [실습] AOP - 따라하기

---
+++ Project 03/aop-sandbox/src/main/java/com/example/aopsandbox/AopSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.aopsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopSandboxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AopSandboxApplication.class, args);
+  }
+
+}
---
+++ Project 03/aop-sandbox/src/test/java/com/example/aopsandbox/AopSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aopsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopSandboxApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

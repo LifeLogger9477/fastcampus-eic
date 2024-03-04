3dfae21cdb65a884f1af87906282cc59b1c4f875
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-03-04 12:13:58 +0900

    Project 03. Ch 02. 05. [실습] 쿼리 결과를 Java 객체로 변환하기 - 람다식 사용

--- Project 03/aop-sandbox/multi-module-a/src/main/java/com/example/multimodulea/repository/DummyJdbcTemplateRepository.java
+++ Project 03/aop-sandbox/multi-module-a/src/main/java/com/example/multimodulea/repository/DummyJdbcTemplateRepository.java
@@ -2,13 +2,11 @@ package com.example.multimodulea.repository;
 
 import com.example.multimodulea.entity.Dummy;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -57,18 +55,16 @@ public class DummyJdbcTemplateRepository implements DummyRepository {
 
     String sql = "select * from dummy where name = ?";
     List<Dummy> dummies =
-        jdbcTemplate.query(sql, new RowMapper<Dummy>() {
-          @Override
-          public Dummy mapRow(ResultSet rs, int rowNum) throws SQLException {
+        jdbcTemplate.query(sql, (ResultSet rs, int rowNum) -> {
 
-            Dummy dummy = new Dummy();
+          Dummy dummy = new Dummy();
 
-            dummy.setId(rs.getInt("id"));
-            dummy.setName(rs.getString("name"));
+          dummy.setId(rs.getInt("id"));     // --> columnIndex를 작성해도 된다.
+          dummy.setName(rs.getString("name"));
 
-            return dummy;
-          }
-        }, name);
+          return dummy;
+        },
+            name);
 
     return Optional.ofNullable(dummies.get(0));
   }
